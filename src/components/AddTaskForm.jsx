b6fb65d66bd2f195db79c07e71e95ebdb031e775
SofiaPCHome <shestakova.sofia2016@mail.ru>
2025-11-02 16:00:22 +0400

    start

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,12 @@
+import Field from "./Field.jsx";
+import Button from "./Button.jsx";
+
+const AddTaskForm = () => {
+  return (
+    <form className="todo__form">
+      <Field />
+      <Button />
+    </form>
+  )
+}
+export default AddTaskForm
